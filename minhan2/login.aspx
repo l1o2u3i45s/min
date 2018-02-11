552c16cde5468258a36af0bf483f09484856df5b
xilian <l1o2u3i45s1@gmail.com>
2018-02-11 11:08:20 +0800

    加入專案檔案。

---
+++ minhan2/login.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="login.aspx.cs" Inherits="minhan2.login" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label runat="server" Text="帳號"/>
+            <asp:TextBox id="account" runat="server" />
+            <asp:Label runat="server" Text="密碼"/>
+            <asp:TextBox id="password" runat="server" />
+            <asp:Button  runat="server" Text="login" OnClick="Unnamed_Click"/>
+        </div>
+    </form>
+</body>
+</html>
---
+++ minhan2/login.aspx.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace minhan2
+{
+    public partial class login : System.Web.UI.Page
+    {
+      
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+       
+        protected void Unnamed_Click(object sender, EventArgs e)
+        {
+            if (CheckLogin(account.Text, password.Text)) {
+                Response.Write("<script>alert(\"登入成功!\"); </script>");
+            }
+            else{
+                Response.Write("<script>alert(\"登入失敗QQ\"); </script>");
+            }
+            
+
+
+
+
+        }
+        private bool CheckLogin(string account, string password) {
+            bool check = false;
+            DB conn = new DB(WebConfigurationManager.ConnectionStrings["SQLDB"].ConnectionString.ToString());
+            List<SqlParameter> paramters = new List<SqlParameter>();
+            paramters.Add(new SqlParameter("ACCOUNT", account));
+            paramters.Add(new SqlParameter("PASSWORD", password));
+            DataTable table = conn.ExecuteProc("[test].[dbo].[CHECKLOGIN]", paramters);
+            if (table.Rows.Count != 0) check = true;
+
+            return  check;
+        }
+    }
+}
